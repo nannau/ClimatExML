9fc30ca24e361856a4bb737f921956dc33e340e3
Nic Annau <nannau@uvic.ca>
2023-10-25 20:41:17 -0700

    Implements hyda_instantiate

--- ClimatExML/loader.py
+++ ClimatExML/loader.py
@@ -2,112 +2,77 @@ import torch
 from torch.utils.data import Dataset
 import lightning as pl
 from torch.utils.data import DataLoader
-from ClimatExML.mlclasses import SuperResolutionData
+from ClimatExML.mlclasses import InvariantData, InputVariables
 from pydantic.dataclasses import dataclass
 
 
-class ClimatExMLLoaderHRCov(Dataset):
+# @dataclass
+class ClimatExSampler(Dataset):
+    lr_paths: list
+    hr_paths: list
+    hr_cov_paths: list
+    lr_invariant_paths: list
 
-    def __init__(self, lr_glob, hr_glob, hr_cov_path, lr_invariant) -> None:
-        self.lr_glob = lr_glob
-        self.hr_glob = hr_glob
-        print(hr_cov_path)
-        self.hr_cov = torch.stack([
-            torch.load(path).float() for path in hr_cov_path
-        ])
+    def __init__(self, lr_paths, hr_paths, hr_cov_paths, lr_invariant_paths) -> None:
+        super().__init__()
+        self.lr_paths = lr_paths
+        self.hr_paths = hr_paths
+        self.hr_cov_paths = hr_cov_paths
+        self.lr_invariant_paths = lr_invariant_paths
+
+        self.hr_cov = (
+            torch.stack([torch.load(path).float() for path in self.hr_cov_paths])
+            .unsqueeze(0)
+            .float()
+        )
 
         self.lr_invariant = [
-            torch.tensor(torch.load(path)).float() for path in lr_invariant
+            torch.tensor(torch.load(path)).float() for path in self.lr_invariant_paths
         ]
-    def __post_init__(self):
-        self.hr_cov = self.hr_cov.unsqueeze(0).float()
-        self.lr_invariant = [
-            torch.tensor(torch.load(path)).float() for path in self.lr_invariant
-        ]
-
 
     def __len__(self):
-        return len(self.lr_glob[0])
+        return len(self.lr_paths[0])
 
     def __getitem__(self, idx):
-        pathlist = [torch.load(var[idx]) for var in self.lr_glob]
+        pathlist = [torch.load(var[idx]) for var in self.lr_paths]
         batch_lr_invariant = [self.lr_invariant[0] * torch.ones(pathlist[0].size())]
         pathlist.extend(batch_lr_invariant)
 
         lr = torch.stack(pathlist, dim=1)
-        hr = torch.stack([torch.load(var[idx]) for var in self.hr_glob], dim=1)
+        hr = torch.stack([torch.load(var[idx]) for var in self.hr_paths], dim=1)
         return [lr, hr, self.hr_cov]
 
 
-class ClimatExMLDataHRCov(pl.LightningDataModule):
+class ClimatExLightning(pl.LightningDataModule):
     def __init__(
-        self, data_glob: dict = None, num_workers: int = 24, batch_size: int = None
+        self,
+        train_data,
+        test_data,
+        invariant,
+        num_workers: int = 24,
     ):
         super().__init__()
-        self.data_glob = data_glob
-        self.batch_size = batch_size
+        self.train_data = train_data
+        self.test_data = test_data
+        self.invariant = invariant
         self.num_workers = num_workers
+        self.batch_size = 1
 
-    def setup(self, stage: str):
-        self.train_data = ClimatExMLLoaderHRCov(
-            self.data_glob["lr_train"],
-            self.data_glob["hr_train"],
-            self.data_glob["hr_cov"],
-            self.data_glob["lr_invariant"],
-        )
-
-    def train_dataloader(self):
-        return (
-            DataLoader(
-                self.train_data,
-                batch_size=self.batch_size,
-                shuffle=True,
-                num_workers=self.num_workers,
-                pin_memory=True,
-            ),
-        )
-
-    def test_dataloader(self):
-        # For some reason this can't be a dictionary?
-        return (
-            DataLoader(
-                self.test_data,
-                batch_size=self.batch_size,
-                num_workers=self.num_workers,
-                shuffle=True,
-                pin_memory=True,
-            ),
-        )
-
-
-class ClimatExMLLoader(Dataset):
-    def __init__(self, lr_glob, hr_glob) -> None:
-        self.lr_glob = lr_glob
-        self.hr_glob = hr_glob
-
-    def __len__(self):
-        return len(self.lr_glob[0])
-
-    def __getitem__(self, idx):
-        lr = torch.stack([torch.load(var[idx]) for var in self.lr_glob], dim=1)
-        hr = torch.stack([torch.load(var[idx]) for var in self.hr_glob], dim=1)
-
-        return [lr, hr]
-
-
-class ClimatExMLData(pl.LightningDataModule):
-    def __init__(
-        self, data_glob: dict = None, num_workers: int = 24, batch_size: int = None
+    def setup(
+        self,
+        stage=None,
     ):
-        super().__init__()
-        self.data_glob = data_glob
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-
-    def setup(self, stage: str):
-        self.train_data = ClimatExMLLoader(
-            self.data_glob["lr_train"],
-            self.data_glob["hr_train"],
+        self.train_data = ClimatExSampler(
+            self.train_data.lr_files,
+            self.train_data.hr_files,
+            self.invariant.hr_cov_paths,
+            self.invariant.lr_invariant_paths,
+        )
+        self.test_data = ClimatExSampler(
+            self.test_data.lr_files,
+            self.test_data.hr_files,
+            self.invariant.hr_cov_paths,
+            self.invariant.lr_invariant_paths,
         )
 
     def train_dataloader(self):
