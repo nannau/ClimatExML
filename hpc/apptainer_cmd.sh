3f48d8795c25da93a1ace630067a95b51b87658c
Nic Annau <nicannau@gmail.com>
2023-12-22 14:04:56 -0800

    docker: build and run script

--- hpc/apptainer_cmd.sh
+++
@@ -1,9 +0,0 @@
-unset KUBERNETES_PORT
-
-pip install -e /home/nannau/light_container/ClimatExML
-
-unset KUBERNETES_PORT
-
-mkdir /home/nannau/data
-
-python /home/nannau/light_container/ClimatExML/ClimatExML/train.py
